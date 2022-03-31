c52dcd3a3086be59a0ad7642f9543bfc91e50911
skidabee-pa-pa <kidabee15@gmail.com>
2022-03-31 22:47:35 +0200

    cascade

---
+++ src/main/java/io/taxventures/TaxventuresApplication.java
@@ -0,0 +1,13 @@
+package io.taxventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxventuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/taxventures/TaxventuresApplicationTests.java
@@ -0,0 +1,13 @@
+package io.taxventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxventuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
