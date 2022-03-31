953e869929622f2c2dac007f3bd49d00083eed8b
skidabee-pa-pa <kidabee15@gmail.com>
2022-04-01 00:15:12 +0200

    auth

--- src/main/java/io/taxventures/service/user/UserService.java
+++ src/main/java/io/taxventures/service/user/UserService.java
@@ -26,7 +26,7 @@ public class UserService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository
                 .findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User with provided email cannot be found."));
--- src/test/java/io/taxventures/service/user/UserServiceTest.java
+++ src/test/java/io/taxventures/service/user/UserServiceTest.java
@@ -7,19 +7,26 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.IdGenerator;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 
 public class UserServiceTest {
 
     private final UUID SOME_UUID = UUID.fromString("4249e08b-ea49-42c3-81a4-a3a26cdb1d2c");
     private final String SOME_RAW_PASSWORD = "pass";
     private final String SOME_ENCODED_PASSWORD = "BBB";
+    private final String SOME_USERNAME = "user@exmaple.com";
 
     @Mock UserRepository repository;
     @Mock IdGenerator idGenerator;
@@ -47,4 +54,29 @@ public class UserServiceTest {
         assertEquals(expectedUser, actualUser);
     }
 
+    @Test
+    void userIsNotCreated_when_UsernameIsOccupied() {
+        Mockito.when(repository.existsByEmail(eq(SOME_USERNAME))).thenReturn(true);
+
+        assertThrows(DuplicatedUsernameException.class, () -> userService.registerUser(SOME_USERNAME, "password"));
+
+        Mockito.verify(repository, never()).save(any());
+    }
+
+    @Test
+    void user_isFoundByEmail_when_UserWithProvidedEmailExists() {
+        User expectedUser = new User(SOME_UUID.toString(), SOME_USERNAME, SOME_ENCODED_PASSWORD);
+        Mockito.when(repository.findByEmail(eq(SOME_USERNAME))).thenReturn(Optional.of(expectedUser));
+
+        UserDetails actualUser = userService.loadUserByUsername(SOME_USERNAME);
+
+        assertEquals(actualUser, expectedUser);
+    }
+
+    @Test
+    void throwsUsernameNotFoundException_when_tryingToGetUser_ByNonExistingUsername() {
+        Mockito.when(repository.findByEmail((SOME_USERNAME))).thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername(SOME_USERNAME));
+    }
 }
